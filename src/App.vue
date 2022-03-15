7934fb28e06be7bb8e6295b7a841ae5274b1e27a
Anh Hao <hi@anhhao.me>
2022-03-15 10:08:52 +0700

    Update Vue 3 project structure

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterView } from 'vue-router';
+import DashboardLayout from '../lib/src/layouts/DashboardLayout.vue';
+</script>
+
+<template>
+  <DashboardLayout>
+    <RouterView />
+  </DashboardLayout>
+</template>
